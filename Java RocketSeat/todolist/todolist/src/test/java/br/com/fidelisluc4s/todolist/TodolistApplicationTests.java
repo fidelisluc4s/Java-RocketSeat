6fda307f4fbad90b5e3d46d3901af576ebc20cb6
Lucas Fidelis <134640628+fidelisluc4s@users.noreply.github.com>
2023-10-09 15:28:35 -0300

    Aula01

---
+++ Java RocketSeat/todolist/todolist/src/test/java/br/com/fidelisluc4s/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fidelisluc4s.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
